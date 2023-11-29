1deb050240efbaec6722915b4e6d58b3ea95ee47
rgo555 <95858482+Edgarduba@users.noreply.github.com>
2023-11-29 10:19:58 +0100

    ya no se que hago

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,35 +4,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
-    private Rigidbody2D _rb;
-    [SerializeField]private float _playerSpeed = 5.5f;
-    [SerializeField]private float _jumpForce = 5.5f;
+     private Rigidbody2D _rb;
+    [SerializeField]private float _playerSpeed = 5; 
+    [SerializeField]private float _jumpForce = 5;
     private float _playerInput;
     [SerializeField]private Animator _anim;
 
-    void Awake()
+
+     void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _anim = GetComponentInChildren<Animator>();
-        //Jump();
+        _anim = GetComponentInChildren<Animator>(); 
     }
+
     void Update()
+    {
+        /*if (Input.GetButtonDown("Jump") && GroundSensor._isGrounded) 
+        {
+            Jump();
+        }
+        _anim.SetBool("isJumping", !GroundSensor._isGrounded); */
+    }
+
+    private void FixedUpdate() 
+    {
+        MageMovement();
+    }
+
+    void MageMovement()
     {
         _playerInput = Input.GetAxis("Horizontal");
         _rb.velocity = new Vector2 (_playerInput*_playerSpeed, _rb.velocity.y);
+
+          if(_playerInput > 0) 
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+            _anim.SetBool("isRunning", true);
+        }
+        else if(_playerInput < 0) 
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            _anim.SetBool("isRunning", true);
+        }
+        else 
+        {
+            _anim.SetBool("isRunning", false);
+        }
     }
 
-    void FixedUpdate()
+    /*void Jump() 
     {
-
-    }
-
-
-    //transform.rotation = Quaternion.Euler(0, 0, 0);
-    //_anim.SetBool("isRunning", true);
-    
-
-
-
+        _rb.AddForce(Vector2.up *_jumpForce, ForceMode2D.Impulse);
+    }*/
 }
